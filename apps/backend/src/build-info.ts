2aa9115eddf5fb1b050044988baa077c0f86ac98
Sarwagya Singh <sarwagyasingh69@gmail.com>
2025-11-26 12:32:50 +0100

    fix(backend): add customers tag to Swagger docs

--- apps/backend/src/build-info.ts
+++ apps/backend/src/build-info.ts
@@ -8,6 +8,6 @@ export const BUILD_INFO = {
   ok: true,
   version: "2025.11.26",
   buildEnv: "development",
-  commitHash: "3ab72a0",
-  buildDate: "2025-11-26T11:27:29.385Z",
+  commitHash: "6774b96",
+  buildDate: "2025-11-26T11:32:42.163Z",
 };
